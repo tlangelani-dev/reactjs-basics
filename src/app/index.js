2508a1495c9fb2c3873792c2fa25e7d00b35c44d
Tlangelani Mboweni <mbowenit@gmail.com>
2017-04-11 21:35:00 +0200

    add App component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,16 @@
-console.log('REACTJS BASICS!');
+import React from 'react';
+import { render } from 'react-dom';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="col-xs-10 col xs-offset-1">
+                    <h1>Welcome to Reactjs Basics</h1>
+                </div>
+            </div>
+        );
+    }
+}
+
+render(<App />, document.getElementById('app'));
