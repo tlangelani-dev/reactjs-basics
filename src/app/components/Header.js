7c20461a5c04e89e077744e723342a0dc68af9f1
Tlangelani Mboweni <mbowenit@gmail.com>
2017-04-11 21:47:22 +0200

    add Header and Home components

---
+++ src/app/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div className="navbar navbar-default">
+                <div className="container">
+                    <div className="navbar-header">
+                        <ul className="nav navbar-nav">
+                            <li>
+                                <a href="#">Home</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+module.exports = Header;
---
+++ src/app/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div id="home">
+                <p>Home Component!</p>
+            </div>
+        );
+    }
+}
+
+module.exports = Home;
